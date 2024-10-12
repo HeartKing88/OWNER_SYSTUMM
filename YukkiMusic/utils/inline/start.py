a4b071c5a146ac03fc6371318d58302149df659e
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2024-10-12 11:35:42 +0530

    ᴠɪᴋᴋʏ
    Signed-off-by: Vikky yadav  <146602351+HeartKing88@users.noreply.github.com>

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -17,8 +17,9 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_5"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
