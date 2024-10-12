f51e7730bb9666f5f731c3fd04d5fa2c59dc4229
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2024-10-12 09:56:40 +0530

    ᴠɪᴋᴋʏ
    Signed-off-by: Vikky yadav  <146602351+HeartKing88@users.noreply.github.com>

--- config/config.py
+++ config/config.py
@@ -96,7 +96,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vivekkumar-IN/YukkiMusic",
+    "https://github.com/HeartKing88/OWNER_SYSTUMM",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
